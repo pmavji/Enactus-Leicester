5c25c85d185ecc1985982d76ff918b3ef64ca514
Amir Sabra <Amir Sabra>
2016-02-21 21:50:33 +0000

    updated main website

---
+++ Main Website Template/js/email.js
@@ -0,0 +1,43 @@
+function email1() {
+  alert("Email: ");
+}
+
+function email2() {
+  alert("Email: pmavji@gmail.com");
+}
+
+function email3() {
+  alert("Email: ");
+}
+
+function email4() {
+  alert("Email: ");
+}
+
+function email5() {
+  alert("Email: ");
+}
+
+function email6() {
+  alert("Email: ");
+}
+
+function email7() {
+  alert("Email: ");
+}
+
+function email8() {
+  alert("Email: ");
+}
+
+function email9() {
+  alert("Email: ");
+}
+
+function email10() {
+  alert("Email: ");
+}
+
+function email11() {
+  alert("Email: amsabra@gmail.com");
+}
