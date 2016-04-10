4423a877b957c2afeb46a27a8df3837809572aa5
Amir Sabra <Amir Sabra>
2016-04-10 12:41:50 +0100

    Updated main website

--- Main Website Template/mail/mail10.php
+++ Main Website Template/mail/mail10.php
@@ -15,7 +15,7 @@ $email_address = $_POST['email10'];
 $message = $_POST['message10'];
 	
 // Create the email and send the message
-$to = 'pmavji@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'revive@enactusleicester.co uk'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nFull Name: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From:$email_address\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
